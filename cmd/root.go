f43c96cbedcf9f301cbf803a10e5467cb7fa55de
Sergey Polivin <s.polivin@gmail.com>
2025-09-01 20:24:54 +0200

    feat(cmd): add version flag for primary command

--- cmd/root.go
+++ cmd/root.go
@@ -4,14 +4,23 @@ Copyright © 2025 Sergey Polivin <s.polivin@gmail.com>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spolivin/jobtracker/version"
 )
+
+var versionFlag bool
 
 var rootCmd = &cobra.Command{
 	Use:   "jobtracker",
 	Short: "Job tracker CLI for tracking job applications",
+	Run: func(cmd *cobra.Command, args []string) {
+		if versionFlag {
+			fmt.Println("Job Tracker CLI", version.Version)
+		}
+	},
 }
 
 func Execute() {
@@ -21,5 +30,5 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&versionFlag, "version", "v", false, "Show version information")
 }
