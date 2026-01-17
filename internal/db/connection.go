648490236e00cdad349bde1c3c5b9c46e5c79131
Sergey Polivin <s.polivin@gmail.com>
2026-01-17 15:14:20 +0100

    feat(db-connection): remove dependency on env variables and modify connection to use connection config and password

--- internal/db/connection.go
+++ internal/db/connection.go
@@ -7,64 +7,43 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"os"
+	"strconv"
+	"time"
 
 	_ "github.com/lib/pq"
+	"github.com/spolivin/jobtracker/v2/internal/db/config"
 )
 
-var DB *sql.DB
-
-// InitDatabase initializes the database connection using environment variables.
-func InitDatabase(ctx context.Context) error {
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+// Connect connects to the Postgres database using config and password.
+func Connect(ctx context.Context, cfg *config.ConnectionConfig, password string) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable connect_timeout=15",
+		cfg.DBHost,
+		strconv.Itoa(cfg.DBPort),
+		cfg.DBUser,
+		password,
+		cfg.DBName,
 	)
 	var err error
-	DB, err = sql.Open("postgres", connStr)
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if err := DB.PingContext(ctx); err != nil {
-		return err
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
 	}
-	return nil
-}
-
-// EnsureSchema creates necessary tables if they do not exist.
-func EnsureSchema(ctx context.Context) error {
-	schema := `
-	CREATE TABLE IF NOT EXISTS applications (
-		id SERIAL PRIMARY KEY,
-		company VARCHAR(255) NOT NULL,
-		position VARCHAR(255) NOT NULL,
-		status VARCHAR(255) NOT NULL,
-		created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-		updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-	);`
-	_, err := DB.ExecContext(ctx, schema)
-	return err
-}
-
-// CloseDatabaseConnection closes the database connection.
-func CloseDatabaseConnection() error {
-	if DB != nil {
-		return DB.Close()
-	}
-	return nil
+	return db, nil
 }
 
 // CheckTableExists checks if a table exists in the database.
-func CheckTableExists(ctx context.Context, tableName string) (bool, error) {
+func CheckTableExists(ctx context.Context, db *sql.DB, tableName string) (bool, error) {
 	var exists bool
 	query := `SELECT EXISTS (
 		       SELECT 1 FROM information_schema.tables 
 		       WHERE table_schema = 'public' AND table_name = $1
 	       )`
-	if err := DB.QueryRowContext(ctx, query, tableName).Scan(&exists); err != nil {
+	if err := db.QueryRowContext(ctx, query, tableName).Scan(&exists); err != nil {
 		return false, err
 	}
 	return exists, nil
