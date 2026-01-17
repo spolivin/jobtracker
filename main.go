64ac4259ade89a39887001ba003641bf44e61b7c
Sergey Polivin <s.polivin@gmail.com>
2026-01-17 15:21:27 +0100

    feat(main): remove CLI dependency upon dotenv files

--- main.go
+++ main.go
@@ -4,11 +4,9 @@ Copyright © 2026 Sergey Polivin <s.polivin@gmail.com>
 package main
 
 import (
-	"github.com/joho/godotenv"
 	"github.com/spolivin/jobtracker/v2/cmd"
 )
 
 func main() {
-	_ = godotenv.Load()
 	cmd.Execute()
 }
